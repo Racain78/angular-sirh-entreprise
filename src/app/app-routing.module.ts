f3f9d4e7af7639ad15e949de9b53837dbf663b7e
Racain78 <hakim.belfkih@lepoles.com>
2018-12-19 17:15:51 +0100

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashbordComponent } from './dashbord/dashbord.component'
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: 'heroes',component: HeroesComponent },
+ {path: 'dashbord',component: DashbordComponent },
+ {path: 'details/:id',component: HeroDetailsComponent},
+ {path: '', redirectTo: '/dashbord', pathMatch: 'full'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+ imports: [RouterModule.forRoot(routes)],
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
